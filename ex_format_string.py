93e6b2e233e541883daa8c005d83bb841fb273b7
Chou Hsiu-fen <amy@server.amy-leon.idv.tw>
2018-09-22 11:15:40 +0800

    done typing, comment out f-string to run through

--- ex_format_string.py
+++ ex_format_string.py
@@ -13,3 +13,19 @@ for key, value in drink_menu.items():
 print('='*30)
 
 # (1) %operator
+for key, value in drink_menu.items():
+    print('%20s: $%5.0f' % (key, value))
+
+# (2) str.format
+for key, value in drink_menu.items():
+    print('{0:<20s}: ${1:>5,.0f}'.format(key, value))
+print('='*30)
+
+# (3) f-string (Python 3.6+)
+#for key, value in drink_menu.items():
+#    print(f'{key:<20s}: ${value:>5,.0f}')
+#print('='*30)
+
+# what's this?
+#for key, value in drink_menu.items():
+#    print(f'{key:<20s}, {value:>05d})
